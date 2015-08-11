ee2b5c0536f95cc93eb4f81beeb86bd2d47987ce
Ainege <Ainege@gmail.com>
2015-08-11 19:57:56 +0300

    validate email

--- app/src/main/java/ru/android/ainege/contactlist/ui/ContactFragment.java
+++ app/src/main/java/ru/android/ainege/contactlist/ui/ContactFragment.java
@@ -3,6 +3,7 @@ package ru.android.ainege.contactlist.ui;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,13 +11,19 @@ import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import ru.android.ainege.contactlist.R;
 
-public class ContactFragment extends Fragment{
+public class ContactFragment extends Fragment {
     public static final String NAME = "name";
     public static final String SURNAME = "surname";
     public static final String EMAIL = "email";
+
+    private EditText mName;
+    private EditText mSurname;
+    private EditText mEmail;
+    private ImageView mAvatar;
 
     public static ContactFragment newInstance(String name, String surname, String email) {
         Bundle args = new Bundle();
@@ -39,21 +46,24 @@ public class ContactFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_contact, container, false);
 
-        EditText name = (EditText) v.findViewById(R.id.name_editText);
-        EditText surname = (EditText) v.findViewById(R.id.surname_editText);
-        EditText email = (EditText) v.findViewById(R.id.email_editText);
+        mName = (EditText) v.findViewById(R.id.name_editText);
+        mSurname = (EditText) v.findViewById(R.id.surname_editText);
+        mEmail = (EditText) v.findViewById(R.id.email_editText);
+        mAvatar = (ImageView) v.findViewById(R.id.avatar);
 
         if (getArguments() != null) {
-            name.setText(getArguments().getString(NAME));
-            surname.setText(getArguments().getString(SURNAME));
-            email.setText(getArguments().getString(EMAIL));
+            mName.setText(getArguments().getString(NAME));
+            mSurname.setText(getArguments().getString(SURNAME));
+            mEmail.setText(getArguments().getString(EMAIL));
         }
 
         Button ok = (Button) v.findViewById(R.id.ok_button);
         ok.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                slideDown();
+                if (saveData()) {
+                    slideDown();
+                }
             }
         });
 
@@ -65,6 +75,27 @@ public class ContactFragment extends Fragment{
             }
         });
         return v;
+    }
+
+    private boolean saveData() {
+        if (isValid()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean isValid() {
+        String email = mEmail.getText().toString().trim();
+        if (!TextUtils.isEmpty(email) && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            if (mEmail.getError() != null) {
+                mEmail.setError(null);
+            }
+            return true;
+        } else {
+            mEmail.setError(getResources().getText(R.string.error_email));
+            return false;
+        }
     }
 
     private void slideDown() {
